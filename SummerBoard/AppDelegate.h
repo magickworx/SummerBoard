0210b315013aa2258d16f4d08682909ab09a12bb
Kouichi ABE (WALL) <kouichi@magickworx.com>
2013-08-19 14:44:24 +0900

    Initial Commit

---
+++ SummerBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SummerBoard
+//
+//  Created by Kouichi ABE (WALL) on 2013/08/19.
+//  Copyright (c) 2013年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SummerBoard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SummerBoard
+//
+//  Created by Kouichi ABE (WALL) on 2013/08/19.
+//  Copyright (c) 2013年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
